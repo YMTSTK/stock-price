bb54b17966f8a90fddea83813eb4f8b7f1e729d1
YMTSTK <87373329+YMTSTK@users.noreply.github.com>
2023-01-04 18:08:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,jsonify
+import numpy as np
+import pickle
+from sklearn import preprocessing
+
+model = pickle.load(open('stock.pkl','rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/stock',methods=['POST'])
+def stock_price():
+
+    open=request.form.get('open')
+    high=request.form.get('high')
+    low=request.form.get('low')
+    volume=request.form.get('volume')
+    stock=request.form.get('stock')
+
+    list=[open,high,low,volume,stock]
+
+    result = model.predict([list])[0]
+
+    return jsonify({'prediction': str(result)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
